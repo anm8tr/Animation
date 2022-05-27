4d0be97635f0ed5473203d1b9771583d7f95df93
Richard Garrison <anm8tr@users.noreply.github.com>
2022-05-26 17:54:39 -0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Richard Garrison on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
