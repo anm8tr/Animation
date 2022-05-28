2c3eb718552be2a6b66aca031ee53f519771d019
Richard Garrison <anm8tr@users.noreply.github.com>
2022-05-27 19:42:00 -0700

    Added custom fonts & dynamic text styles

--- Animation/OnboardingView.swift
+++ Animation/OnboardingView.swift
@@ -14,23 +14,40 @@ struct OnboardingView: View {
     var body: some View {
         ZStack {
             background
-            button.view()
-                .frame(width: 236, height: 64)
-                .overlay(
-                    Label("Start the course", systemImage: "arrow.forward")
-                        .offset(x: 4, y: 4)
-                        .font(.headline)
-                ) // MARK: - ADD DROP SHADOW TO BUTTON
-                .background(
-                    Color.black
-                .cornerRadius(30)
-                .blur(radius: 10)
-                .opacity(0.3)
-                )
-             // MARK: - ADD GESTURE TO TRIGGER ANIMATION
-                .onTapGesture {
-                    button.play(animationName: "active")
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Learn design & code")
+                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                    .frame(width: 260, alignment: .leading)
+                Text("Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.")
+                    .customFont(.body)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Start the course", systemImage: "arrow.forward")
+                            .offset(x: 4, y: 4)
+                            .font(.headline)
+                    ) // MARK: - ADD DROP SHADOW TO BUTTON
+                    .background(
+                        Color.black
+                    .cornerRadius(30)
+                    .blur(radius: 10)
+                    .opacity(0.3)
+                    )
+                 // MARK: - ADD GESTURE TO TRIGGER ANIMATION
+                    .onTapGesture {
+                        button.play(animationName: "active")
                 }
+                Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.")
+                    .customFont(.footnote)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(40)
+            .padding(.top, 40)
                  
             
         } // MARK: - END ZSTACK
